eecabc282784226a44893abe3fd8716b0d50509c
Kapil Rawal <142970092+KapilRawal798@users.noreply.github.com>
2023-11-06 15:49:32 +0530

    Add files via upload
    This is a Practice tutorial

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var tl =  gsap.timeline();
+tl.to(".page1",{
+    y:"100vh",
+    scale:0.5,
+    duration:0,
+})
+tl.to(".page1",{
+    y:"20vh",
+    duration:2,
+   delay:1,
+
+})
+tl.to(".page1",{
+    y:"0vh",
+    scale:1,
+    rotate:"-360",
+    duration:1.5,
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
